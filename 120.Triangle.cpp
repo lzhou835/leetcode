4f05d7239551e1c52a653d2f3c19cabe60b31376
lzhou835 <lzhou835@gmail.com>
2018-06-06 18:23:50 -0700

    120

---
+++ 120.Triangle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<int> dp(triangle.back());
+        int n=triangle.size();
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++)
+                dp[j]=min(dp[j], dp[j+1])+triangle[i][j];
+        }
+        return dp[0];
+    }
+};
