e8f482f5a86435f6fd9b74b6fb78d8e455065f32
lzhou835 <lzhou835@gmail.com>
2018-06-12 22:50:40 -0700

    377

---
+++ 377.Combination_Sum_IV.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1, 0);
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(auto n:nums){
+                if(i>=n) dp[i]+=dp[i-n];
+            }
+        }
+        return dp.back();
+    }
+};
