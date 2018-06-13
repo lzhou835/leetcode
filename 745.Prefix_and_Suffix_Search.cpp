23cffa4c621091d89a647c41f3e6f1ac00ba8ffe
lzhou835 <lzhou835@gmail.com>
2018-06-13 16:20:10 -0700

    745

---
+++ 745.Prefix_and_Suffix_Search.cpp
@@ -0,0 +1,24 @@
+class WordFilter {
+public:
+    WordFilter(vector<string> words) {
+        for(int k=0;k<words.size();k++){
+            for(int i=0;i<=words[k].size();i++){
+                for(int j=0;j<=words[k].size();j++){
+                    m[words[k].substr(0, i)+"#"+words[k].substr(words[k].size()-j)]=k;
+                }
+            }
+        }
+    }
+    
+    int f(string prefix, string suffix) {
+        return m.count(prefix+"#"+suffix)==1?m[prefix+"#"+suffix]:-1;
+    }
+private:
+    unordered_map<string, int> m;
+};
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter obj = new WordFilter(words);
+ * int param_1 = obj.f(prefix,suffix);
+ */
