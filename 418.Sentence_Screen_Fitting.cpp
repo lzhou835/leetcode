b268d0fd8d3e8920460c641ac53393403dd36afb
lzhou835 <lzhou835@gmail.com>
2018-07-01 00:45:32 -0700

    418

---
+++ 418.Sentence_Screen_Fitting.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int wordsTyping(vector<string>& sentence, int rows, int cols) {
+        string all="";
+        for(string s:sentence) all+=s+" ";
+        int start=0, len=all.size();
+        
+        for(int i=0;i<rows;i++){
+            start+=cols;
+            if(all[start%len]==' '){
+                start++;
+            }else{
+                while(start>0 && all[(start-1)%len]!=' '){
+                    start--;
+                }
+            }
+        }
+        
+        return start/len;
+    }
+};
