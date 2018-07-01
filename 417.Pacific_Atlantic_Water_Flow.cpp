8a79f071ec4eb00c7f233da80249bb7e63c71771
lzhou835 <lzhou835@gmail.com>
2018-06-30 22:37:00 -0700

    417

---
+++ 417.Pacific_Atlantic_Water_Flow.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void solve(vector<vector<int>> & matrix, vector<vector<bool>> & visited, int pre, int i, int j){
+        int m=matrix.size(), n=matrix[0].size();
+        if(i<0 || i>=m || j<0 || j>=n || visited[i][j] || matrix[i][j]<pre) return;
+        visited[i][j]=true;
+        solve(matrix, visited, matrix[i][j], i-1, j);
+        solve(matrix, visited, matrix[i][j], i, j-1);
+        solve(matrix, visited, matrix[i][j], i+1, j);
+        solve(matrix, visited, matrix[i][j], i, j+1);
+    }
+    
+    vector<pair<int, int>> pacificAtlantic(vector<vector<int>>& matrix) {
+        if(matrix.empty() || matrix[0].empty()) return {};
+        vector<pair<int, int>> res;
+        int m=matrix.size(), n=matrix[0].size();
+        vector<vector<bool>> pacific(m, vector<bool>(n, false));
+        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
+        for(int i=0;i<m;i++){
+            solve(matrix, pacific, INT_MIN, i, 0);
+            solve(matrix, atlantic, INT_MIN, i, n-1);
+        }
+        
+        for(int i=0;i<n;i++){
+            solve(matrix, pacific, INT_MIN, 0, i);
+            solve(matrix, atlantic, INT_MIN, m-1, i);
+        }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(pacific[i][j] && atlantic[i][j]){
+                    res.push_back({i, j});
+                }
+            }
+        }
+        
+        return res;
+    }
+};
