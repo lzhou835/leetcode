c5a38c30213284788bfe51f217eb605e4b24aff0
lzhou835 <lzhou835@gmail.com>
2018-08-08 16:09:25 -0700

    62

---
+++ 62.Unique_Paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(m<1 || n<1) return 0;
+        vector<int> path(n, 1);
+
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                path[j]=path[j]+path[j-1];
+            }
+        }
+        return path[n-1];
+    }
+};
+
