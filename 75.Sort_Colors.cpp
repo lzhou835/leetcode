5e32f74dfef78a571d1445ab617e4d19847906a5
lzhou835 <lzhou835@gmail.com>
2018-08-16 14:43:39 -0700

    75

---
+++ 75.Sort_Colors.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int red=0, blue=n-1;
+        for(int i=0;i<=blue;i++){
+            if(nums[i]==0) swap(nums[i], nums[red++]);
+            else if(nums[i]==2) swap(nums[i--], nums[blue--]);
+        }
+    }
+};
